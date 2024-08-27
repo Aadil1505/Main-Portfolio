40b5ab80702f1b88f1e7946d3459e715f1f94478
Aadil M. Alli <aalli4@pride.hofstra.edu>
2024-08-28 01:06:28 +0300

    Meta test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,21 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="en" suppressHydrationWarning>
+        <head>
+<meta property="og:url" content="https://portfolio-test-theta.vercel.app/"/>
+<meta property="og:type" content="website"/>
+<meta property="og:title" content="Aadil's Portfolio"/>
+<meta property="og:description" content="A simple portfolio site explaining me, my work, and passion."/>
+<meta property="og:image" content="https://opengraph.b-cdn.net/production/images/690b884d-209f-4b7c-ac46-e66fc5a9f2e5.png?token=YbadBs_Ji6btCRS3NA9aQnn1RmKjpR6EGcX17RsyqpA&height=675&width=1200&expires=33260796001"/>
+
+<meta name="twitter:card" content="summary_large_image"/>
+<meta property="twitter:domain" content="portfolio-test-theta.vercel.app"/>
+<meta property="twitter:url" content="https://portfolio-test-theta.vercel.app/"/>
+<meta name="twitter:title" content="Aadil's Portfolio"/>
+<meta name="twitter:description" content="A simple portfolio site explaining me, my work, and passion."/>
+<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/690b884d-209f-4b7c-ac46-e66fc5a9f2e5.png?token=YbadBs_Ji6btCRS3NA9aQnn1RmKjpR6EGcX17RsyqpA&height=675&width=1200&expires=33260796001"/>
+
+        </head>
         <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
               <DockDemo />
